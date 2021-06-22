50de2a0edd082cf19b6e7a24dae8353afb0ee5d6
PCH <qhqhahah@naver.com>
2021-06-22 21:32:38 +0900

    rs PlanarSurface Study

---
+++ 03.rsApplication/PlanarSurface/PlanarSurface.py
@@ -0,0 +1,27 @@
+import rhinoscriptsyntax as rs
+
+
+pts = []
+lns = []
+cen = scale / 2
+
+for i in range(2):
+    for j in range(2):
+        p = rs.AddPoint(i * scale - cen, j * scale - cen, 0)
+        p = rs.PointCoordinates(p)
+        pts.append(p)
+
+for j in range(3):
+    if j % 2 == 0:
+        ln = rs.AddLine(pts[j], pts[j+1])
+        lns.append(ln)
+
+for k in range(2):
+    ln = rs.AddLine(pts[k], pts[k+2])
+    lns.append(ln)
+
+srf = rs.AddPlanarSrf(lns)
+
+#a = pts
+#b = lns
+c = srf
