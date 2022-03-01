9f0455c48c74be1635a1158fe2a7d6d551c938f6
PARKCHEOLHEE-lab <83874157+PARKCHEOLHEE-lab@users.noreply.github.com>
2022-03-01 17:24:48 +0900

    Add files via upload

---
+++ 04.rsExercise/MatrixStep/MatrixStep.py
@@ -0,0 +1,21 @@
+﻿import Rhino.Geometry as rg
+import Rhino.RhinoDoc as rc
+import rhinoscriptsyntax as rs
+import ghpythonlib.components as gh
+
+matrix_step = []
+
+for i in range(maxtrix_count):
+    z = i * height
+    
+    for j in range(maxtrix_count):
+        step_base = rg.Point3d(i*scale, j*scale, z)
+        step_rec = gh.Rectangle(step_base, scale, scale, 0).rectangle
+        step_srf = gh.BoundarySurfaces(step_rec)
+        step_vec = rg.Point3d(0, 0, -z)
+        
+        step = gh.Extrude(step_srf, step_vec)
+        
+        z = z + height
+        
+        matrix_step.append(step)
