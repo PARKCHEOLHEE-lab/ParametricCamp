83cd44c9080ab0ff49060f0bec216d7b637a45dd
PARKCHEOLHEE-lab <83874157+PARKCHEOLHEE-lab@users.noreply.github.com>
2022-05-05 16:37:42 +0900

    Add files via upload

---
+++ 04.rsExercise/WaveCircle/WaveCircle.py
@@ -0,0 +1,13 @@
+﻿import Rhino.Geometry as rg
+import rhinoscriptsyntax as rs
+import ghpythonlib.components as gh
+
+origin = rg.Point3d(150,0,0)
+circle = rg.Circle(origin, radius)
+divide_pts = gh.DivideCurve(circle, divide_count, False).points
+
+moved_divide_pts = []
+for i, p in enumerate(divide_pts):
+    if i % 2 == 0:
+        rs.MoveObject(p, [0,0,radius/5])
+
