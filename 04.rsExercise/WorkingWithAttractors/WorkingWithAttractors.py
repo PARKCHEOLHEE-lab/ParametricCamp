590cc1e8b667b510113dffd36a8be7e5ff8a2c08
PARKCHEOLHEE-lab <83874157+PARKCHEOLHEE-lab@users.noreply.github.com>
2022-05-21 15:02:31 +0900

    Add files via upload

---
+++ 04.rsExercise/WorkingWithAttractors/WorkingWithAttractors.py
@@ -0,0 +1,22 @@
+﻿import random
+import Rhino.Geometry as rg
+import ghpythonlib.components as gh
+
+O = 1
+PLANE = rg.Plane.WorldXY
+
+hexagon_cells, hexagon_points = gh.Hexagonal(plane=PLANE,
+                                             size=O,
+                                             extent_x=cells,
+                                             extent_y=cells-3)
+
+circles = []
+vectors = []
+for hexagon_point in hexagon_points:
+    normal = attractor - hexagon_point
+    circle = gh.CircleCNR(center=hexagon_point, normal=normal, radius=O/1.2)
+    circle_surface = gh.BoundarySurfaces(edges=circle)
+    
+    circles.append(circle_surface)
+    vectors.append(normal)
+
